ea8136baf12a01d918e84a51e0db625a8a2615dc
Li Haoyi <haoyi@dropbox.com>
2014-03-11 21:31:53 -0700

    Added a wrapper making XMLHttpRequests much less painful; as time goes on eventually we'll have a similar wrapper for Websockets and other such APIs

--- src/main/scala/org/scalajs/dom/extensions/Extensions.scala
+++ src/main/scala/org/scalajs/dom/extensions/Extensions.scala
@@ -147,3 +147,53 @@ object KeyCode {
   val y = 89
   val z = 90
 }
+
+import scala.concurrent.{Promise, Future}
+/**
+ * Thrown when `Ajax.get` or `Ajax.post` receives a non-20X response code.
+ * Contains the XMLHttpRequest that resulted in that respons
+ */
+case class AjaxException(xhr: dom.XMLHttpRequest) extends Exception
+/**
+ * Wraps an XMLHttpRequest to provide an easy one-line way of making 
+ * an Ajax call, returning a Future.
+ */
+object Ajax{
+  def get(url: String,
+          data: String = "",
+          timeout: Int = 0,
+          headers: Seq[(String, String)] = Nil,
+          withCredentials: Boolean = false) = {
+    apply("GET", url, data, timeout, headers, withCredentials)
+  }
+  def post(url: String,
+           data: String = "",
+           timeout: Int = 0,
+           headers: Seq[(String, String)] = Nil,
+           withCredentials: Boolean = false) = {
+    apply("POST", url, data, timeout, headers, withCredentials)
+  }
+  def apply(method: String,
+            url: String,
+            data: js.String,
+            timeout: Int,
+            headers: Seq[(String, String)],
+            withCredentials: Boolean): Future[dom.XMLHttpRequest] = {
+    val req = new dom.XMLHttpRequest()
+    val promise = Promise[dom.XMLHttpRequest]
+    req.withCredentials = withCredentials
+
+    req.onreadystatechange = {(e: dom.Event) =>
+      if (req.readyState.toInt == 4){
+        if (200 <= req.status && req.status < 300)
+          promise.success(req)
+        else
+          promise.failure(AjaxException(req))
+      }
+    }
+    req.open(method, url)
+    headers.foreach(x => req.setRequestHeader(x._1, x._2))
+    req.send(data)
+    promise.future
+  }
+}
