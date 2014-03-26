5236a6acc7f295951a54d775c0d28256d207dbc1
antonkulaga <antonkulaga@gmail.com>
2014-03-26 16:14:47 +0200

    useful methods for any ScalaJS object

--- src/main/scala/org/scalajs/dom/extensions/package.scala
+++ src/main/scala/org/scalajs/dom/extensions/package.scala
@@ -3,6 +3,7 @@ package org.scalajs.dom
 import scala.scalajs.js
 import org.scalajs.dom
 import scala.collection.mutable
+import scala.scalajs.js.Undefined
 
 package object extensions {
 
@@ -72,27 +73,64 @@ package object extensions {
     }
   }
 
+  /**
+   * Implicit class that adds some useful methods to any ScalaJS object
+   * @param obj any ScalaJS object
+   */
+  implicit class AnyJs(obj:scalajs.js.Any){
+    /**
+     * As Javascript has both null and undefined often if(element.obj==null) returns false when obj is undefined,
+     * it is especially  unpleasant with dom wrappers
+     * @param other another object to compare to
+     * @return
+     */
+    def ===(other:Any): Boolean = if(other==null) obj==null || obj.isInstanceOf[Undefined] || obj=="" else obj==other
+
+    /**
+     * Just a shorter conversion to dynamic object
+     * @return self as Dynamic
+     */
+    def dyn = obj.asInstanceOf[js.Dynamic]
+
+    /**
+     * provides dynamic results as options
+     * @param key name of the property
+     * @return Option[js.Dynamic]
+     */
+    def \ (key:String): Option[js.Dynamic] = dyn.selectDynamic(key) match {
+      case null=>
+        dom.document.getElementById("").parentElement.tagName
+        None
+      case v:Undefined=>None
+      case validValue=>Some(validValue)
+    }
+  }
+
+  /**
+   * Useful for complicated traversals, like
+   *  grandfather \ "mother" \ "daughter"
+   * @param opt option with Dynamic object
+   */
+  implicit class OptionPath(opt:Option[js.Dynamic]){
+    def \ (key:String): Option[js.Dynamic] = opt.flatMap(_ \ key)
+  }
 
   /**
    * Implicit class to deal with attributes as with normal mutable Map
-   * @param attributes
+   * @param attributes dom Attributes
    */
   implicit class Attributes(attributes:NamedNodeMap) extends mutable.Map[String,Attr] {
     self =>
 
     override def iterator: Iterator[(String, Attr)] = new Iterator[(String, Attr)] {
       var index = 0
-
-
       override def next(): (String, Attr) = {
         val n: Attr = attributes.item(index)
         this.index = this.index + 1
         (n.name, n)
       }
-
       override def hasNext: Boolean = index < self.length
     }
-
 
     override def get(key: String): Option[Attr] = attributes.getNamedItem(key) match {
       case null => None
@@ -100,7 +138,6 @@ package object extensions {
     }
 
     def length: Int = attributes.length.toInt
-
 
     override def -=(key: String) = {
       attributes.removeNamedItem(key)
@@ -111,6 +148,5 @@ package object extensions {
       attributes.setNamedItem(kv._2)
       this}
   }
-
 
 }
