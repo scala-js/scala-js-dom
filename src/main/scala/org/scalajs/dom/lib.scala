c00a05198c29d98716024c698788c9a874447c9e
wwagner4 <wwagner4@gmail.com>
2014-05-21 15:58:06 +0200

    Added the Touch class and references

--- src/main/scala/org/scalajs/dom/lib.scala
+++ src/main/scala/org/scalajs/dom/lib.scala
@@ -2195,15 +2195,137 @@ class TouchList extends js.Object {
    *
    * MDN
    */
-  def item(index: js.Number): TouchEvent = ???
+  def item(index: js.Number): Touch = ???
 
   @scala.scalajs.js.annotation.JSBracketAccess
-  def apply(index: js.Number): TouchEvent = ???
+  def apply(index: js.Number): Touch = ???
 
   @scala.scalajs.js.annotation.JSBracketAccess
   def update(index: js.Number, v: Node): Unit = ???
 }
 
+/**
+ * A Touch object represents a single point of contact between the user and a touch-sensitive
+ * interface device (which may be, for example, a touchscreen or a trackpad).
+ *
+ * Note: Many of these values are hardware-dependent; for example, if the device doesn't have a
+ * way to detect the amount of pressure placed on the surface, the force value will always be 1.0.
+ * This may also be the case for radiusX and radiusY; if the hardware reports only a single point,
+ * these values will be 1.
+ *
+ * MDN
+ */
+class Touch extends js.Object {
+
+  /**
+   * A unique identifier for this Touch object. A given touch (say, by a finger) will have the same
+   * identifier for the duration of its movement around the surface. This lets you ensure that you're
+   * tracking the same touch all the time. Read only.
+   *
+   * MDN
+   */
+  def identifier: js.Number = ???
+
+  /**
+   * The X coordinate of the touch point relative to the left edge of the screen.
+   * Read only.
+   *
+   * MDN
+   */
+  def screenX: js.Number = ???
+
+  /**
+   * The Y coordinate of the touch point relative to the top edge of the screen.
+   * Read only.
+   *
+   * MDN
+   */
+  def screenY: js.Number = ???
+
+  /**
+   * The X coordinate of the touch point relative to the left edge of the browser viewport,
+   * not including any scroll offset. Read only.
+   *
+   * MDN
+   */
+  def clientX: js.Number = ???
+
+  /**
+   * The Y coordinate of the touch point relative to the top edge of the browser viewport,
+   * not including any scroll offset. Read only.
+   *
+   * MDN
+   */
+  def clientY: js.Number = ???
+
+  /**
+   *  The X coordinate of the touch point relative to the left edge of the document. Unlike clientX,
+   *  this value includes the horizontal scroll offset, if any.
+   *
+   * MDN
+   *  Read only.
+   */
+  def pageX: js.Number = ???
+
+  /**
+   * The Y coordinate of the touch point relative to the top of the document. Unlike clientY, this value
+   * includes the vertical scroll offset, if any.
+   * Read only.
+   *
+   * MDN
+   */
+  def pageY: js.Number = ???
+
+  /**
+   * The X radius of the ellipse that most closely circumscribes the area of contact with the screen.
+   * The value is in pixels of the same scale as screenX.
+   * Read only.
+   *
+   * MDN
+   */
+  def radiusX: js.Number = ???
+
+  /**
+   * The Y radius of the ellipse that most closely circumscribes the area of contact with the screen.
+   * The value is in pixels of the same scale as screenY.
+   * Read only.
+   *
+   * MDN
+   */
+  def radiusY: js.Number = ???
+
+  /**
+   * The angle (in degrees) that the ellipse described by radiusX and radiusY must be rotated,
+   * clockwise, to most accurately cover the area of contact between the user and the surface.
+   * Read only.
+   *
+   * MDN
+   */
+  def rotationAngle: js.Number = ???
+
+  /**
+   * The amount of pressure being applied to the surface by the user, as a float
+   * between 0.0 (no pressure) and 1.0 (maximum pressure).
+   * Read only.
+   *
+   * MDN
+   */
+  def force: js.Number = ???
+
+  /**
+   * The Element on which the touch point started when it was first placed on the surface,
+   * even if the touch point has since moved outside the interactive area of that element or
+   * even been removed from the document. Note that if the target is removed from the document,
+   * events will still be targeted at it, and hence won't necessarily bubble up to the window
+   * or document anymore. If there's any risk of an element being removed while it is being
+   * touched, best practice is to attach the touch listeners directly to the target.
+   * Read only.
+   *
+   * MDN
+   */
+  def target: EventTarget = ???
+
+}
 /**
  * KeyboardEvent objects describe a user interaction with the keyboard. Each event
  * describes a key; the event type (keydown, keypress, or keyup) identifies what
