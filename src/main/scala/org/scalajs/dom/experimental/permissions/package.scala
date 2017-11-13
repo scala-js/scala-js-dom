b1ad221be23d61b4993e832ace1a511b246049a6
Greg Dorrell <Greg@Dorrell.me>
2017-11-13 13:13:01 +0000

    Partially address code review feedback.

--- src/main/scala/org/scalajs/dom/experimental/permissions/package.scala
+++ src/main/scala/org/scalajs/dom/experimental/permissions/package.scala
@@ -1,10 +1,10 @@
 package org.scalajs.dom.experimental
 
-import language.implicitConversions
+import scala.language.implicitConversions
+
 import org.scalajs.dom
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.ScalaJSDefined
 
 /**
   * Implements the Permissions API.
@@ -16,27 +16,27 @@ package object permissions {
   @js.native
   trait PermissionState extends js.Any
 
-  final val `granted` = "granted".asInstanceOf[PermissionState]
-  final val `denied` = "denied".asInstanceOf[PermissionState]
-  final val `prompt` = "prompt".asInstanceOf[PermissionState]
+  object PermissionState {
+    final val granted = "granted".asInstanceOf[PermissionState]
+    final val denied = "denied".asInstanceOf[PermissionState]
+    final val prompt = "prompt".asInstanceOf[PermissionState]
+  }
 
-  @ScalaJSDefined
   trait PermissionStatus extends dom.raw.EventTarget {
     val state: PermissionState
     val onchange: js.Function1[PermissionState, _]
   }
 
-  @ScalaJSDefined
   trait Permissions extends js.Any {
     def query(permissionDescriptor: js.Object): js.Promise[PermissionStatus]
   }
 
-  @ScalaJSDefined
   trait PermissionsNavigator extends js.Any {
     val permissions: Permissions
   }
 
-  implicit def toPermissions(navigator: dom.raw.Navigator): PermissionsNavigator =
+  implicit def toPermissions(
+      navigator: dom.raw.Navigator): PermissionsNavigator =
     navigator.asInstanceOf[PermissionsNavigator]
 
 }
