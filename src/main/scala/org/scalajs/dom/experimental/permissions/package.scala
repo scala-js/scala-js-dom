bedef218f583c45c716fec210e80c990fbedfae9
Greg Dorrell <Greg@memimo.co.uk>
2017-11-13 19:12:50 +0000

    Add basic support for the Permissions API (#299)

---
+++ src/main/scala/org/scalajs/dom/experimental/permissions/package.scala
@@ -0,0 +1,68 @@
+package org.scalajs.dom.experimental
+
+import scala.language.implicitConversions
+
+import org.scalajs.dom
+
+import scala.scalajs.js
+
+/**
+ * Implements the Permissions API.
+ *
+ * [[https://www.w3.org/TR/permissions/ W3C Working Draft]]
+ */
+package object permissions {
+
+  @js.native
+  sealed trait PermissionState extends js.Any
+
+  object PermissionState {
+    val granted = "granted".asInstanceOf[PermissionState]
+    val denied = "denied".asInstanceOf[PermissionState]
+    val prompt = "prompt".asInstanceOf[PermissionState]
+  }
+
+  trait PermissionStatus extends dom.raw.EventTarget {
+    val state: PermissionState
+    val onchange: js.Function1[PermissionState, _]
+  }
+
+  @js.native
+  sealed trait PermissionName extends js.Any
+
+  object PermissionName {
+    val geolocation = "geolocation".asInstanceOf[PermissionName]
+    val midi = "midi".asInstanceOf[PermissionName]
+    val notifications = "notifications".asInstanceOf[PermissionName]
+    val push = "push".asInstanceOf[PermissionName]
+    val `persistent-storage` =
+      "persistent-storage".asInstanceOf[PermissionName]
+  }
+
+  trait PermissionDescriptor extends js.Object {
+    val name: PermissionName
+  }
+
+  object PermissionDescriptor {
+    @inline
+    def apply(permissionName: PermissionName): PermissionDescriptor = {
+      new PermissionDescriptor {
+        val name = permissionName
+      }
+    }
+  }
+
+  trait Permissions extends js.Object {
+    def query(
+        permissionDescriptor: PermissionDescriptor): js.Promise[PermissionStatus]
+  }
+
+  trait PermissionsNavigator extends js.Object {
+    val permissions: Permissions
+  }
+
+  implicit def toPermissions(
+      navigator: dom.raw.Navigator): PermissionsNavigator =
+    navigator.asInstanceOf[PermissionsNavigator]
+
+}
