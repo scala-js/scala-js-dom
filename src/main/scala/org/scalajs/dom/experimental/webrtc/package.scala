50ee72c2a28f6520fc04463be880c1f2963619d5
Roberto Tyley <roberto.tyley@theguardian.com>
2018-11-29 16:39:55 +0000

    Add `navigator.mediaDevices.getUserMedia()`, deprecate old method
    The `navigator.getUserMedia()` method is deprecated:
    
    https://developer.mozilla.org/en-US/docs/Web/API/Navigator/getUserMedia
    
    ...and in fact during hacking [1], I found that I could gain Firefox
    support just by using the `navigator.mediaDevices.getUserMedia()` method
    instead, which is documented as having good cross-browser support:
    
    https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia#Browser_compatibility
    
    The deprecation seems to have occurred around 28th May 2014:
    
    https://github.com/w3c/mediacapture-main/commit/de3bdaf83d2cc1350cc3be970baf1d11219add1d#diff-ea76d38900f79cfae8f60e5f7cf16dd1R2755
    
    [1] https://twitter.com/rtyley/status/1067815067780087808

--- src/main/scala/org/scalajs/dom/experimental/webrtc/package.scala
+++ src/main/scala/org/scalajs/dom/experimental/webrtc/package.scala
@@ -12,6 +12,23 @@ package object webrtc {
   @js.native
   trait NavigatorMediaStream extends js.Object {
 
+    /**
+     * The MediaDevices getUserMedia() method prompts the user for permission to use a media input
+     * which produces a MediaStream with tracks containing the requested types of media. That stream
+     * can include, for example, a video track (produced by either a hardware or virtual video source
+     * such as a camera, video recording device, screen sharing service, and so forth), an audio track
+     * (similarly, produced by a physical or virtual audio source like a microphone, A/D converter, or
+     * the like), and possibly other track types.
+     *
+     * MDN
+     *
+     * @see [[https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia]]
+     */
+    def mediaDevices: MediaDevices
+
+    @deprecated(
+        "Navigator.getUserMedia() is deprecated, use Navigator.mediaDevices.getUserMedia() instead.",
+        "2014-05-28")
     def getUserMedia(constraints: MediaStreamConstraints,
         success: js.Function1[MediaStream, Any],
         error: js.Function1[DOMError, Any]): Unit = js.native
