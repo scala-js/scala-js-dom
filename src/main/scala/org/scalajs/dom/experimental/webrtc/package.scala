4cf1503be7aafc04b8ff570c74bb86ab6ec21bd1
Roberto Tyley <roberto.tyley@gmail.com>
2018-11-30 10:58:03 +0000

    Add `navigator.mediaDevices.getUserMedia()`, deprecate old method (#342)
    The `navigator.getUserMedia()` method is deprecated:
    https://developer.mozilla.org/en-US/docs/Web/API/Navigator/getUserMedia
    
    The deprecation seems to have occurred around 28th May 2014:
    https://github.com/w3c/mediacapture-main/commit/de3bdaf83d2cc1350cc3be970baf1d11219add1d#diff-ea76d38900f79cfae8f60e5f7cf16dd1R2755

--- src/main/scala/org/scalajs/dom/experimental/webrtc/package.scala
+++ src/main/scala/org/scalajs/dom/experimental/webrtc/package.scala
@@ -12,6 +12,19 @@ package object webrtc {
   @js.native
   trait NavigatorMediaStream extends js.Object {
 
+    /**
+     * The Navigator.mediaDevices read-only property returns a MediaDevices object, which provides access to connected
+     * media input devices like cameras and microphones, as well as screen sharing.
+     *
+     * MDN
+     *
+     * @see [[https://developer.mozilla.org/en-US/docs/Web/API/Navigator/mediaDevices]]
+     */
+    def mediaDevices: MediaDevices = js.native
+
+    @deprecated(
+        "Navigator.getUserMedia() is deprecated, use Navigator.mediaDevices.getUserMedia() instead.",
+        "2014-05-28")
     def getUserMedia(constraints: MediaStreamConstraints,
         success: js.Function1[MediaStream, Any],
         error: js.Function1[DOMError, Any]): Unit = js.native
