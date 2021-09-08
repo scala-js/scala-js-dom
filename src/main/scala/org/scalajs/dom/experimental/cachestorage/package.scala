da06c4aa32527c649dd346ab49d84b019cac9dcb
Arman Bilge <armanbilge@gmail.com>
2021-09-08 02:27:44 +0000

    Migrate CacheStorage to dom

---
+++ src/main/scala/org/scalajs/dom/experimental/cachestorage/package.scala
@@ -0,0 +1,14 @@
+package org.scalajs
+package dom.experimental
+
+package object cachestorage {
+
+  @deprecated("use dom.CacheQueryOptions instead", "2.0.0")
+  type CacheQueryOptions = dom.CacheQueryOptions
+
+  @deprecated("use dom.CacheStorage instead", "2.0.0")
+  type CacheStorage = dom.CacheStorage
+
+  @deprecated("use dom.Cache instead", "2.0.0")
+  type Cache = dom.Cache
+}
