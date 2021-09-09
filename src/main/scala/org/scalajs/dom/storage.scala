e99445a2fd8bd630d53f315a7f7bd1a352625f47
Arman Bilge <armanbilge@gmail.com>
2021-09-09 00:38:00 +0000

    Extend js.Object instead of js.Any

--- src/main/scala/org/scalajs/dom/storage.scala
+++ src/main/scala/org/scalajs/dom/storage.scala
@@ -3,14 +3,14 @@ package org.scalajs.dom
 import scala.scalajs.js
 
 @js.native
-trait StorageManager extends js.Any {
+trait StorageManager extends js.Object {
   def persisted(): js.Promise[Boolean] = js.native
   def persist(): js.Promise[Boolean] = js.native
   def estimate(): js.Promise[StorageEstimate] = js.native
 }
 
 @js.native
-trait StorageEstimate extends js.Any {
+trait StorageEstimate extends js.Object {
   val usage: Double = js.native
   val quota: Double = js.native
 }
