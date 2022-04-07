8e3c7dab5200a5d2238a66e1a02706336dd3ce3f
Arman Bilge <armanbilge@gmail.com>
2022-04-07 13:51:15 +0000

    Fix WorkerOptions encoding

--- dom/src/main/scala/org/scalajs/dom/WorkerOptions.scala
+++ dom/src/main/scala/org/scalajs/dom/WorkerOptions.scala
@@ -2,9 +2,8 @@ package org.scalajs.dom
 
 import scala.scalajs.js
 
-@js.native
-trait WorkerOptions extends js.Any {
-  var credentials: js.UndefOr[RequestCredentials] = js.native
-  var name: js.UndefOr[String] = js.native
-  var `type`: js.UndefOr[WorkerType] = js.native
+trait WorkerOptions extends js.Object {
+  var credentials: js.UndefOr[RequestCredentials] = js.undefined
+  var name: js.UndefOr[String] = js.undefined
+  var `type`: js.UndefOr[WorkerType] = js.undefined
 }
