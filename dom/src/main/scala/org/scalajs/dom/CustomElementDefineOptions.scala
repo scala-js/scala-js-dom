3fba3892de892f9a6fcb08efae99c650e7ee0d34
叶子航 <jerryzihye@outlook.com>
2022-04-24 17:52:17 +0800

    Partial support for custom elements

---
+++ dom/src/main/scala/org/scalajs/dom/CustomElementDefineOptions.scala
@@ -0,0 +1,8 @@
+package org.scalajs.dom
+
+import scala.scalajs.js
+
+/** A CustomElementDefineOptions object represents additional options associated with CustomElementRegsitry.define. */
+trait CustomElementDefineOptions extends js.Object {
+  var `extends`: js.UndefOr[Boolean] = js.undefined
+}
---
+++ dom/src/main/scala/org/scalajs/dom/CustomElementRegistry.scala
@@ -0,0 +1,21 @@
+/** All documentation for facades is thanks to Mozilla Contributors at https://developer.mozilla.org/en-US/docs/Web/API
+  * and available under the Creative Commons Attribution-ShareAlike v2.5 or later.
+  * http://creativecommons.org/licenses/by-sa/2.5/
+  *
+  * Everything else is under the MIT License http://opensource.org/licenses/MIT
+  */
+package org.scalajs.dom
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation._
+
+/** The CustomElementRegistry interface provides methods for registering custom elements and querying registered
+  * elements. To get an instance of it, use the window.customElements property.
+  */
+@js.native
+@JSGlobal
+abstract class CustomElementRegistry extends js.Object {
+
+  /** Defines a new custom element. */
+  def define(name: String, constructor: js.Dynamic, options: CustomElementDefineOptions = js.native): Unit
+}
