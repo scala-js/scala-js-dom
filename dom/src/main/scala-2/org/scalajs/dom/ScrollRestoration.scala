c11aedf7bcd0a0c46c723246e99033ee28e3b086
zetashift <rskaraya@gmail.com>
2022-08-21 01:34:37 +0200

    Also scaladoc comment for the scala 2 enum

--- dom/src/main/scala-2/org/scalajs/dom/ScrollRestoration.scala
+++ dom/src/main/scala-2/org/scalajs/dom/ScrollRestoration.scala
@@ -10,6 +10,8 @@ sealed trait ScrollRestoration extends js.Any
   * which contains the spec for ScrollRestoration
   */
 object ScrollRestoration {
+  /** The location on the page to which the user has scrolled will be restored. */
   val auto: ScrollRestoration = "auto".asInstanceOf[ScrollRestoration]
+  /** The location on the page is not restored. The user will have to scroll to the location manually. */
   val manual: ScrollRestoration = "manual".asInstanceOf[ScrollRestoration] 
 }
