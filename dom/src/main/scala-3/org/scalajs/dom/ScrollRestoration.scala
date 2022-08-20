a64b7c28880a1de8b3ad5a10690cbe5bc6d08eae
zetashift <rskaraya@gmail.com>
2022-08-20 13:10:53 +0200

    Better doc comments for Scroll Restoration
    Co-authored-by: Arman Bilge <armanbilge@gmail.com>

--- dom/src/main/scala-3/org/scalajs/dom/ScrollRestoration.scala
+++ dom/src/main/scala-3/org/scalajs/dom/ScrollRestoration.scala
@@ -9,6 +9,8 @@ opaque type ScrollRestoration <: String = String
   * which contains the spec for ScrollRestoration
   */
 object ScrollRestoration {
+  /** The location on the page to which the user has scrolled will be restored. */
   val auto: ScrollRestoration = "auto"
+  /** The location on the page is not restored. The user will have to scroll to the location manually. */
   val manual: ScrollRestoration = "manual" 
 }
