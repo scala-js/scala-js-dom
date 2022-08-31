cdf7d8f33b01b4371641880aac81dceddb093226
Arman Bilge <armanbilge@gmail.com>
2022-08-31 02:50:48 +0000

    Update to 2.12.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   object Ver {
     val scala211 = "2.11.12"
-    val scala212 = "2.12.14"
+    val scala212 = "2.12.15"
     val scala213 = "2.13.6"
     val scala3   = "3.1.3"
   }
