2311de1eacbb34f61cffe31222d0386318ef78bb
Arman Bilge <armanbilge@gmail.com>
2023-02-09 20:26:22 +0000

    Formatting

--- dom/src/main/scala/org/scalajs/dom/Audio.scala
+++ dom/src/main/scala/org/scalajs/dom/Audio.scala
@@ -9,7 +9,9 @@ package org.scalajs.dom
 import scala.scalajs.js
 import scala.scalajs.js.annotation._
 
-/** creates and returns a new HTMLAudioElement which can be either attached to a document for the user to interact with and/or listen to, or can be used offscreen to manage and play audio. */
+/** creates and returns a new HTMLAudioElement which can be either attached to a document for the user to interact with
+  * and/or listen to, or can be used offscreen to manage and play audio.
+  */
 @JSGlobal
 @js.native
 class Audio extends HTMLAudioElement {
