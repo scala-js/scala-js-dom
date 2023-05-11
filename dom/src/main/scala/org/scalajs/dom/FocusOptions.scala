fcc918bf4fa6578c9890963e963fe0eb03004826
Christoph Bunte <christophbunte@gmail.com>
2023-05-11 18:05:03 +0200

    make FocusOptions extend js.Object

--- dom/src/main/scala/org/scalajs/dom/FocusOptions.scala
+++ dom/src/main/scala/org/scalajs/dom/FocusOptions.scala
@@ -2,7 +2,7 @@ package org.scalajs.dom
 
 import scala.scalajs.js
 
-trait FocusOptions {
+trait FocusOptions extends js.Object {
   var preventScroll: js.UndefOr[Boolean] = js.undefined
   var focusVisible: js.UndefOr[Boolean] = js.undefined
 }
