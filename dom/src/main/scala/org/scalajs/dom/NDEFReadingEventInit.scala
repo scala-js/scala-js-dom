5170f893488458d238afec5cbc4b06f221ba971e
zetashift <rskaraya@gmail.com>
2023-12-21 23:48:59 +0100

    fix: NDEFReadingEventInit.serialNumber returns a string

--- dom/src/main/scala/org/scalajs/dom/NDEFReadingEventInit.scala
+++ dom/src/main/scala/org/scalajs/dom/NDEFReadingEventInit.scala
@@ -15,7 +15,7 @@ trait NDEFReadingEventInit extends EventInit {
   /** A string with the name of the event. It is case-sensitive and browsers always set it to reading. Default is "" an
     * empty string
     */
-  var serialNumber: js.UndefOr[AbortSignal] = js.undefined
+  var serialNumber: js.UndefOr[String] = js.undefined
 
   /** An object that, in addition of the properties defined in Event(), can have the following properties: serialNumber;
     * message
