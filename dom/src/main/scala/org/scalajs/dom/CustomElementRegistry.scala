f6df22595ee9788d3c564b2f8c86bbbc9917fdc7
cyz1901 <1093491992@qq.com>
2024-01-14 20:15:45 +0800

    add get and getName function

--- dom/src/main/scala/org/scalajs/dom/CustomElementRegistry.scala
+++ dom/src/main/scala/org/scalajs/dom/CustomElementRegistry.scala
@@ -11,10 +11,15 @@ import scala.scalajs.js.annotation._
 
 /** The CustomElementRegistry interface provides methods for registering custom elements and querying registered
   * elements. To get an instance of it, use the window.customElements property.
+  * https://developer.mozilla.org/en-US/docs/Web/API/CustomElementRegistry
   */
 @js.native
 @JSGlobal
 abstract class CustomElementRegistry extends js.Object {
+
+  def get(name: String): js.Dynamic
+
+  def getName(constructor: js.Dynamic): String
 
   /** Defines a new custom element. */
   def define(name: String, constructor: js.Dynamic, options: ElementDefinitionOptions = js.native): Unit
