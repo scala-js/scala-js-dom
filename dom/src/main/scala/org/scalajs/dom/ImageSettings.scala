773c6ed834909461e057ed5815c3b4b9223ab628
c <git@actorgrid.net>
2024-03-18 07:51:10 -0600

    prePR
    Added ImageSettings Object with static values for sRGB and displayP3.

--- dom/src/main/scala/org/scalajs/dom/ImageSettings.scala
+++ dom/src/main/scala/org/scalajs/dom/ImageSettings.scala
@@ -5,3 +5,8 @@ import scala.scalajs.js
 trait ImageSettings extends js.Object {
   var colorSpace: js.UndefOr[String] = js.undefined
 }
+
+object ImageSettings {
+  lazy val sRGB: ImageSettings = js.Dynamic.literal(colorSpace = "srgb").asInstanceOf[ImageSettings]
+  lazy val displayP3: ImageSettings = js.Dynamic.literal(colorSpace = "display-p3").asInstanceOf[ImageSettings]
+}
